166a75d4a2a50c7eeb01fac6b7a98b64ec3a891c
Zachary Clark <zacclark369@gmail.com>
2021-11-30 17:05:39 -0800

    add ability to edit task descriptions

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,6 @@ var loadTasks = function() {
 
   // loop over object properties
   $.each(tasks, function(list, arr) {
-    console.log(list, arr);
     // then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
@@ -45,7 +44,44 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+$(".list-group").on("click", p, function() {
+  var text = $(this)
+  .text()
+  .trim();
+  var textInput = $("<textarea></textarea>").addClass("form-control")
+  .val(text);
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+});
 
+$(".list-group").on("blur", "textarea", function() {
+// get the textarea's current value/text
+var text = $(this)
+.val()
+.trim();
+
+// get the parent ul's id attribute
+var status = $(this)
+.closest(".list-group")
+.attr("id")
+.replace("list-", "");
+
+// get the task's position in the list of other li elements
+var index = $(this)
+.closest(".list-group-item")
+.index()
+
+tasks[status][index].text = text;
+saveTasks();
+
+// recreate p elememnt
+var taskP = $("<p>")
+.addClass("m-1")
+.text(text);
+
+// replace texttarea with p element element
+$(this).replaceWith(taskP);
+});
 
 
 // modal was triggered
